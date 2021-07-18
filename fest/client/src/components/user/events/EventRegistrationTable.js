48446e1d6b01a54ea5cfdbf72d20aa0c5fcc8cc7
“Rahul <rahulgupta6542@gmail.com>
2021-07-18 12:55:38 +0530

    event registration table

---
+++ fest/client/src/components/user/events/EventRegistrationTable.js
@@ -0,0 +1,233 @@
+import React from "react";
+import EventRegistrationTableCheckbox from "./EventRegistrationTableCheckbox";
+
+function EventRegistrationTable() {
+  return (
+    <>
+      <div className="table-wrapper">
+        <table className="fl-table">
+          <thead>
+            <tr>
+              <th>Event</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Dicount</th>
+              <th>Register</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>Carrom</td>
+              <td>31st December 2021</td>
+              <td>10</td>
+              <td>10%</td>
+              <td>
+                {" "}
+                <EventRegistrationTableCheckbox />{" "}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="event__registration_submit_div">
+        <div>
+          <button className="event__registration_submit">
+            submit
+          </button> 
+        </div>       
+      </div>
+    </>
+  );
+}
+
+export default EventRegistrationTable;
